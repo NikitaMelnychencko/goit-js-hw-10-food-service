3759ad00d4e1c7b5ed7f7dd0aadaf9d06890500d
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-07 17:45:12 +0300

    add_Theme

--- src/index.js
+++ src/index.js
@@ -2,20 +2,40 @@ import './sass/main.scss';
 import menu from './menu.json'
 import dishesCards from './templates/dishes-cards.hbs'
 
-// menu-toggle
-// const user = {
-//   name: 'Mango',
-//   age: 2,
-// };
-// console.log(JSON.stringify(user));
-// const savedUserData = '{"name":"Mango","age":2}';
-// console.log(JSON.parse(savedUserData));
-// console.log(localStorage);
-// localStorage.setItem('my-data',JSON.stringify(user))
-// const saveData = localStorage.getItem('my-data')
-// const parsedData = JSON.parse(saveData)
-// console.log('parsedData',parsedData);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-const refMenu = document.querySelector('.js-menu');
+const refBody = document.querySelector('body')
+const refCheckBox = refBody.querySelector('.theme-switch__control')
+const refInput = refCheckBox.querySelector('.theme-switch__toggle')
+const refMenu = refBody.querySelector('.js-menu');
+
+refInput.setAttribute("checked", "true")
+const localStorageValue = localStorage.getItem('Theme')
+//check Local Storage 
+if (localStorageValue==='dark-theme') {
+  refInput.checked = false
+  refBody.classList.add(Theme.DARK)
+} else {
+ refBody.classList.add(Theme.LIGHT) 
+}
+//theme switcher
+refCheckBox.addEventListener('change', e => {
+  if (e.target.nodeName!=='INPUT') {
+    return
+  }
+  refBody.classList.toggle(Theme.LIGHT)
+  refBody.classList.toggle(Theme.DARK)
+
+  if (refBody.className==='dark-theme') {
+   localStorage.setItem('Theme', 'dark-theme') 
+  }else{
+    localStorage.removeItem('Theme')
+  }
+  
+})
+//templating and gallery parsing
 const markup = dishesCards(menu);
 refMenu.insertAdjacentHTML('beforeend',markup)
